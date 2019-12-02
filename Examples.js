c3385ed33d06fab85fd97b97882e496b08b83aaf
Sagar MAhindrakar <mahindra.sag@gmail.com>
2019-12-02 19:07:48 +0530

    Update Examples.js

--- Examples.js
+++ Examples.js
@@ -322,6 +322,34 @@ const flatten = arr => arr.reduce(
 
 console.log( flatten([1, [2,3], [4,[5],[6,[7,8,9],10],11],[12],13]) );
 
+//filter array in range
+function filterRange(arr, a, b) {
+    // added brackets around the expression for better readability
+    return arr.filter(item => (a <= item && item <= b));
+  }
+  
+  let arr = [5, 3, 8, 1];
+  
+  let filtered = filterRange(arr, 1, 4);
+  
+  alert( filtered ); // 3,1 (matching values)
+  
+  alert( arr ); // 5,3,8,1 (not modified)
+
+//capitlize first word
+function camelize(str) {
+  return str
+    .split('-') // splits 'my-long-word' into array ['my', 'long', 'word']
+    .map(
+      // capitalizes first letters of all array items except the first one
+      // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
+      (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
+}
+camelize("background-color") == 'backgroundColor';
+camelize("list-style-image") == 'listStyleImage';
+camelize("-webkit-transition") == 'WebkitTransition';
 
 //check if array,null,object,fucntionsa
 if( Object.prototype.toString.call( arrayList ) === '[object Array]' ) {
